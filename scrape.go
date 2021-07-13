cd85cbb57ebccb76a7d099340600aea7ed92b675
bsimps01 <b.albertsimpson@gmail.com>
2021-07-13 16:43:07 -0700

    added adjustments to scrape.go

--- scrape.go
+++ scrape.go
@@ -6,18 +6,34 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type Article struct {
+	ID       string `json:"id"`
+	HEADLINE string `json:"headline"`
+	TITLE    string `json:"title"`
+	LINK     string `json:"link"`
+}
+
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
+
+	//allIDs := make([]Article, 0)
 	// Instantiate default collector
 	c := colly.NewCollector()
+	stories := []Article{}
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	c.OnHTML(".l-grid__content > a", func(e *colly.HTMLElement) {
+		//link := e.Attr("href")
+
+		story := Article{}
+		story.HEADLINE = e.ChildText(".l-grid__content-title u-text-h5")
+		story.TITLE = e.ChildText(".p-headlkine-stack__link")
+		story.LINK = e.ChildText(".p-headline-stack__link")
+		stories = append(stories, story)
 
 		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		fmt.Printf("Headline: %q\n, Title: %q\n, Link: %q\n, ", story.HEADLINE, story.TITLE, story.LINK)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -26,5 +42,5 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("http://www.mlb.com/")
 }
